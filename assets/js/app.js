5dca112c1f0bbd6d9d3d4fba6e8894b958938289
Monir <sgtmonirh@gmail.com>
2020-04-29 19:51:11 +0600

    Virsion 1

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+( function( $ ) {
+	// Global variables
+    var $win = $(window);
+	
+	$('#year').text(new Date().getFullYear());
+    var $scrollTopBtn = $('#scrollTopBtn');
+    // Show Scroll Top Button
+    $win.on('scroll', function () {
+        if ($(this).scrollTop() > 700) {
+            $scrollTopBtn.addClass('actived');
+        } else {
+            $scrollTopBtn.removeClass('actived');
+        }
+    });
+    // Animate Body after Clicking on Scroll Top Button
+    $scrollTopBtn.on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+} )( jQuery );
